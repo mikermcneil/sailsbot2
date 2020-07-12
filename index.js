44890f4a9c1c3fc3769a3acc7a06f700808c169b
Mike McNeil <mikermcneil@users.noreply.github.com>
2020-07-12 18:31:49 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+
+{// Nursery
+  let x = 5;
+
+  // Corpus of training data
+  // (situations, by name)
+  let stnsByName = {
+    [Date.now()]: [],
+    [Date.now()]: [],
+    [Date.now()]: [],
+    [Date.now()]: [],
+    [Date.now()]: [],
+  };
+}//∫
+
+
+
+let notice, decide;
+{// Brain
+
+  let getMemory, setMemory;
+  {// Memories
+    let memoriesByMrl = {};
+    getMemory = (mrl)=>{
+
+    };
+    setMemory = (/* ? */)=>{
+
+    };
+  }//∫
+
+  notice = (mrl)=>{
+
+  };
+
+  decide = (/* ? */)=>{
+
+  };
+
+}//∫
+
+
+
+// old
+
+// let nursery = (()=>{
+// …
+
+//   return {
+//     enter: ()=>{},
+//     leave: ()=>{},
+//   };
+// })();//eﬁi
+
+
+// let brain = (()=>{
+
+// })();//eﬁi
+
+// * …
+
+// † …
+
+// ‡ …
+
