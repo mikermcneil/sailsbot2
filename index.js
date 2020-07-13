df64fb7a3399e7bdccadb7cf9cdf3645495666e9
Mike McNeil <mikermcneil@users.noreply.github.com>
2020-07-12 19:44:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 
+let train;
 {// Nursery
-  let x = 5;
-
-  // Corpus of training data
-  // (situations, by name)
-  let stnsByName = {
-    [Date.now()]: [],
-    [Date.now()]: [],
-    [Date.now()]: [],
-    [Date.now()]: [],
-    [Date.now()]: [],
+  // Corpus of training situations,
+  // each consisting of pre-recorded stimuli
+  let situationsByName = {
+    'Unit 1 · Coffee makers': [{},{}],
+    'Unit 2 · Opening doors': [{},{}],
+    'Unit 3 · Footsteps': [{},{}],
+    'Unit 4 · Voices': [{},{}],
   };
+
+  train = ()=>{
+
+  };
+
 }//∫
 
 
@@ -41,21 +44,6 @@ let notice, decide;
 
 
 
-// old
-
-// let nursery = (()=>{
-// …
-
-//   return {
-//     enter: ()=>{},
-//     leave: ()=>{},
-//   };
-// })();//eﬁi
-
-
-// let brain = (()=>{
-
-// })();//eﬁi
 
 // * …
 
